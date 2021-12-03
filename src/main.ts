456bd126c31ac6e2cb2c9c9116137c9dedbc83a7
Parthasai <53685926+propardhu@users.noreply.github.com>
2021-12-03 16:33:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Textarea from 'primevue/textarea';
+import Button from 'primevue/button';
+
+const app = createApp(App);
+
+app.component('Textarea', Textarea);
+app.component('Button', Button);
 
 
-createApp(App).mount('#app')
+app.mount('#app')
 
 
